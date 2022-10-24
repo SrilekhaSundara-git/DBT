0c8ad27cfe35ce1978fba6bc61c0c85f35d92214
srilekha.sundara@truthics.com <srilekha.sundara@truthics.com>
2022-10-24 12:56:21 +0000

    dbt seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -4,7 +4,7 @@ select
 orderid,
 orderdate,
 shipdate,
-shipmode,
+o.shipmode,
 o.customerid,
 o.productid,
 ordersellingprice,
@@ -18,9 +18,13 @@ category,
 productname,
 subcategory,
 {{ markup('ordersellingprice', 'ordercostprice' ) }} as markup,
-ordersellingprice - ordercostprice as orderprofit
+ordersellingprice - ordercostprice as orderprofit,
+-- from delivery_team
+delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+on o.shipmode = d.shipmode
